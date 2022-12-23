1a023796529afa036d618431ddc91b1aa89a90c7
Mike <mcluck90@gmail.com>
2022-12-23 21:23:41 +0000

    Make constant folding recursive

--- src/compiler/passes/constant-folding.ts
+++ src/compiler/passes/constant-folding.ts
@@ -1,6 +1,8 @@
 import {
+  BinaryExpressionNode,
   createFloatingPointNode,
   createIntegerNode,
+  ExpressionNode,
   isNodeType,
   ProgramNode,
 } from '../../parser/ast'
@@ -10,37 +12,42 @@ export type Input = ProgramNode
 export type Output = ProgramNode
 
 export const constantFolding = (program: Input): Output => {
-  const visitor = new DepthFirstVisitor({
-    visitBinaryExpression(node) {
-      const left = node.left
-      const right = node.right
-      if (isNodeType('Integer')(left) && isNodeType('Integer')(right)) {
-        switch (node.operator) {
-          case '*':
-            return createIntegerNode(left.value * right.value)
-          case '+':
-            return createIntegerNode(left.value + right.value)
-          case '-':
-            return createIntegerNode(left.value - right.value)
-          case '/':
-            return createIntegerNode(Math.floor(left.value / right.value))
-        }
-      } else if (
-        isNodeType('FloatingPoint')(left) &&
-        isNodeType('FloatingPoint')(right)
-      ) {
-        switch (node.operator) {
-          case '*':
-            return createFloatingPointNode(left.value * right.value)
-          case '+':
-            return createFloatingPointNode(left.value + right.value)
-          case '-':
-            return createFloatingPointNode(left.value - right.value)
-          case '/':
-            return createFloatingPointNode(left.value / right.value)
-        }
+  const foldConstants = (node: BinaryExpressionNode): ExpressionNode | void => {
+    const left = isNodeType('BinaryExpression')(node.left)
+      ? foldConstants(node.left)
+      : node.left
+    const right = isNodeType('BinaryExpression')(node.right)
+      ? foldConstants(node.right)
+      : node.right
+    if (isNodeType('Integer')(left) && isNodeType('Integer')(right)) {
+      switch (node.operator) {
+        case '*':
+          return createIntegerNode(left.value * right.value)
+        case '+':
+          return createIntegerNode(left.value + right.value)
+        case '-':
+          return createIntegerNode(left.value - right.value)
+        case '/':
+          return createIntegerNode(Math.floor(left.value / right.value))
       }
-    },
+    } else if (
+      isNodeType('FloatingPoint')(left) &&
+      isNodeType('FloatingPoint')(right)
+    ) {
+      switch (node.operator) {
+        case '*':
+          return createFloatingPointNode(left.value * right.value)
+        case '+':
+          return createFloatingPointNode(left.value + right.value)
+        case '-':
+          return createFloatingPointNode(left.value - right.value)
+        case '/':
+          return createFloatingPointNode(left.value / right.value)
+      }
+    }
+  }
+  const visitor = new DepthFirstVisitor({
+    visitBinaryExpression: foldConstants,
   })
 
   visitor.visitProgram(program)
